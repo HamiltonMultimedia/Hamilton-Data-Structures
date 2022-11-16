5358348bc8f8e3e9b14ff1e977151fcd247c48d5
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-16 16:42:20 -0500

    Added TheArrays Class

---
+++ src/main/java/com/hamilton/TheArrays.java
@@ -0,0 +1,14 @@
+package com.hamilton;
+
+import java.util.Arrays;
+
+public class TheArrays {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "blue";
+        colors[1] = "purple";
+
+        System.out.println(Arrays.toString(colors));
+
+    }
+}
