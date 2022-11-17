1dfa91a738bf67181b9072bd4b7074f14430b572
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-16 19:12:11 -0500

    Updated TheArrays Class

--- src/main/java/com/hamilton/TheArrays.java
+++ src/main/java/com/hamilton/TheArrays.java
@@ -10,5 +10,28 @@ public class TheArrays {
 
         System.out.println(Arrays.toString(colors));
 
+        colors[2] = "yellow";
+
+        int[] numbers = {100, 200};
+
+        // Loop through the array iteratively.
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
+        }
+
+        // Reversed the iteration
+        for (int i = colors.length - 1; i >= 0; i--) {
+            System.out.println(colors[i]);
+        }
+
+        // For-Each loop with colon (:) syntax
+        for (String color : colors) {
+            System.out.println(color);
+        }
+
+        // Using built-in stream() method from Arrays class
+        Arrays.stream(colors).forEach(System.out::println);
+
+
     }
 }
