2bc94d801ceb9bf2d813c151267d5d683783e174
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-20 16:32:44 -0500

    Updated WorkingWithQueues Class

--- src/main/java/com/hamilton/WorkingWithQueues.java
+++ src/main/java/com/hamilton/WorkingWithQueues.java
@@ -10,7 +10,11 @@ public class WorkingWithQueues {
         supermarket.add(new Person("Mariam", 18));
         supermarket.add(new Person("Ali", 40));
 
-        System.out.println(supermarket.peek());
+        System.out.println(supermarket.size() + " is the amount in this queue");
+        System.out.println(supermarket.peek() + " is the object at the top of this queue");
+        System.out.println(supermarket.poll() + " will be removed from this queue");
+        System.out.println(supermarket.size() + " this is the new size of the queue");
+        System.out.println(supermarket.peek() + " is the new object at the top of this queue");
     }
     static record Person(String name, int age) {
 
