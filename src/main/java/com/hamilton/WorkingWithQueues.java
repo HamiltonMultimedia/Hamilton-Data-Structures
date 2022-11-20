70c91e16f44097473487c3b8bf985cf442592980
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-20 16:24:44 -0500

    Added WorkingWithQueues

---
+++ src/main/java/com/hamilton/WorkingWithQueues.java
@@ -0,0 +1,18 @@
+package com.hamilton;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class WorkingWithQueues {
+    public static void main(String[] args) {
+        Queue<Person> supermarket = new LinkedList<>();
+        supermarket.add(new Person("Alex", 21));
+        supermarket.add(new Person("Mariam", 18));
+        supermarket.add(new Person("Ali", 40));
+
+        System.out.println(supermarket.peek());
+    }
+    static record Person(String name, int age) {
+
+    }
+}
