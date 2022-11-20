c4b132b1a061dd4a6add88cee96488704d7e6ff1
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-20 16:12:57 -0500

    Updated WorkingWithStacks Class

--- src/main/java/com/hamilton/WorkingWithStacks.java
+++ src/main/java/com/hamilton/WorkingWithStacks.java
@@ -4,10 +4,12 @@ import java.util.Stack;
 
 public class WorkingWithStacks {
     public static void main(String[] args) {
+        // Stacks in Java use Vectors which use Lists
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         stack.push(2);
         stack.push(3);
-        System.out.println(stack.peek());
+        System.out.println(stack.peek() + " is at the top of this stack");
+        System.out.println(stack.size() + " is the size of this stack");
     }
 }
