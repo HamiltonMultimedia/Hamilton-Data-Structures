541ae6b4154462ece2f6391c539e66800cd52936
HamiltonMultimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2022-11-26 09:38:21 -0500

    Added Diamond object to WorkingWithMaps

--- src/main/java/com/hamilton/WorkingWithMaps.java
+++ src/main/java/com/hamilton/WorkingWithMaps.java
@@ -5,6 +5,15 @@ import java.util.Map;
 
 public class WorkingWithMaps {
     public static void main(String[] args) {
+//        maps();
+        maps();
+        Map<WorkingWithQueues.Person, Diamond> map = new HashMap<>();
+        map.put(new WorkingWithQueues.Person("Victoria", 30), new Diamond("Congo Diamond"));
+        System.out.println(map.get(new WorkingWithQueues.Person("Victoria", 30)));
+        System.out.println(map.entrySet());
+    }
+
+    private static void maps() {
         Map<Integer, WorkingWithQueues.Person> map = new HashMap<>();
         map.put(1, new WorkingWithQueues.Person("Alex", 21));
         map.put(2, new WorkingWithQueues.Person("Alexa", 23));
@@ -12,14 +21,16 @@ public class WorkingWithMaps {
         System.out.println(map);
         System.out.println(map.size());
         System.out.println(map.get(1));
-        System.out.println(map.containsKey(3));
+        System.out.println(map.containsKey(4));
         System.out.println(map.keySet());
         System.out.println(map.entrySet());
         map.entrySet().forEach(x -> System.out.println(x.getKey() + " " + x.getValue()));
         map.forEach((key, person) -> {
             System.out.println(key + " - " + person);
         });
-        System.out.println(map.getOrDefault(4, new WorkingWithQueues.Person("default", 00)));
+        System.out.println(map.getOrDefault(5, new WorkingWithQueues.Person("default", 00)));
         System.out.println(map.values());
     }
+
+    record Diamond(String name){}
 }
